9e59aa9acb5e9e0adbcdffc0f9b2d1490283223a
a1l2e3x42010 <a1l2e3x42010@users.noreply.github.com>
2023-05-19 20:02:57 +0300

    11

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,101 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+let instance;
+
+// Create markup for gallery
+function createItemsGalleryMarkup(galleryItems) {
+  const itemsGalleryMarkup = galleryItems
+    .map(
+      ({ original, preview, description }) =>
+        `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img 
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </li>`
+    )
+    .join("");
+
+  return itemsGalleryMarkup;
+}
+
+// Insert markup to gallery
+galleryContainer.insertAdjacentHTML(
+  "afterbegin",
+  createItemsGalleryMarkup(galleryItems)
+);
+
+// Gallery event listener by click
+galleryContainer.addEventListener('click', (e) => {
+  e.preventDefault();
+  
+  const isGalleryImage = e.target.classList.contains("gallery__image");
+  
+  if (!isGalleryImage) {
+    return;
+  }
+  
+  const { target: galleryImgEl } = e;
+  
+  showModal(galleryImgEl);
+});
+
+
+// Create lightbox and modal
+function showModal({ dataset: { source }, alt }) {
+  instance = basicLightbox.create(`<img src="${source}" alt="${alt}">`, {
+    onShow: openModal,
+    onClose: closeModal,
+  });
+
+  instance.show();
+}
+
+// Before open lightbox
+function openModal() {
+  window.addEventListener("keydown", onKeyPress);
+  bodyScrollLock();
+}
+
+// Before close lightbox
+function closeModal() {
+  window.removeEventListener("keydown", onKeyPress);
+  bodyScrollUnlock();
+}
+
+// lock body scroll 
+function bodyScrollLock() {
+  const body = document.querySelector("body");
+  const bodyStyle = window.getComputedStyle(body);
+  const bodyWidth = body.offsetWidth + parseInt(bodyStyle.marginLeft) + parseInt(bodyStyle.marginRight);
+  const verticalScrollBar = window.innerWidth - bodyWidth;
+
+  body.style.overflow = "hidden";
+  body.style.paddingRight = verticalScrollBar + "px";
+}
+
+// Unlock body scroll 
+function bodyScrollUnlock() {
+  const body = document.querySelector("body");
+
+  setTimeout(() => {
+    body.style.overflow = "auto";
+    body.style.paddingRight = "";
+  }, 250);
+}
+
+// Escape to close box
+function onKeyPress(e) {
+  if (e.code !== "Escape") {
+    return;
+  }
+
+  instance.close();
+}
